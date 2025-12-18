b966ab18c2b9138766c6faddfc7de8e2b036af9d
Tassilo Klein <TJKlein@gmail.com>
2025-12-18 10:25:01 +0100

    Update train_llama2.py

--- train_llama2.py
+++ train_llama2.py
@@ -15,6 +15,11 @@ Usage:
 Authors: Tassilo Klein, Moin Nabi
 License: Apache 2.0
 """
+#
+# SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 # %%
 
 import argparse
